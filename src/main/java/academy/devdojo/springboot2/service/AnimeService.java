26a900248d1cd7cd261a0e86f588ec38355eeff9
Kaue Pereira <kauepereira_@outlook.com>
2024-06-20 20:45:03 -0300

    feat: aula 8

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -1,15 +1,27 @@
 package academy.devdojo.springboot2.service;
 
-import academy.devdojo.springboot2.repository.AnimeRepository;
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.repository.AnimeRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 
 @Service
 public class AnimeService implements AnimeRepository {
+
+    private List<Anime> animes = List.of(new Anime(1L, "Boku no Hero"), new Anime(2L, "Berserk"));
+
     public List<Anime> listAll() {
-        return List.of(new Anime(1L, "Boku no Hero"), new Anime(2L, "Berserk"));
+        return animes;
+    }
+
+    public Anime findById(long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not foud")); //Caso não encontre um id valido
     }
 }
