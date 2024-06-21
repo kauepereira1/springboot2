6353c85e39068f5c287adb57f834b64a794f280f
Kaue Pereira <kauepereira_@outlook.com>
2024-06-21 18:44:05 -0300

    feat: config banco de dados mysql e conexao com jpa, aula 14

--- src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
+++ src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
@@ -1,9 +1,10 @@
 package academy.devdojo.springboot2.repository;
 
 import academy.devdojo.springboot2.domain.Anime;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface AnimeRepository {
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
     List<Anime> listAll();
 }
