3169b3bf8c9367c0f568c5aa99c8514ad09a150c
Kaue Pereira <kauepereira_@outlook.com>
2024-06-21 15:11:34 -0300

    feat: implementando o metodo PUT, aula 11

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -42,4 +42,9 @@ public class AnimeController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @PutMapping
+    public ResponseEntity<Void> replace(@RequestBody Anime anime) {
+        animeService.replace(anime);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -39,4 +39,10 @@ public class AnimeService implements AnimeRepository {
     public void delete(long id) {
         animes.remove(findById(id));
     }
+
+
+    public void replace(Anime anime) {
+        delete(anime.getId());
+        animes.add(anime);
+    }
 }
