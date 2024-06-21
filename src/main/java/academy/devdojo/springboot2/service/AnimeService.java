caec499f50d210c912c9c3d058d2d8790ffd735c
Kaue Pereira <kauepereira_@outlook.com>
2024-06-21 13:53:19 -0300

    feat: adicionando o metodo post, aula 9

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -6,13 +6,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 @Service
 public class AnimeService implements AnimeRepository {
+    private static List<Anime> animes;
 
-    private List<Anime> animes = List.of(new Anime(1L, "Boku no Hero"), new Anime(2L, "Berserk"));
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L, "Boku no Hero"), new Anime(2L, "Berserk")));
+    }
 
     public List<Anime> listAll() {
         return animes;
@@ -23,5 +28,11 @@ public class AnimeService implements AnimeRepository {
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not foud")); //Caso não encontre um id valido
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 1000_0000));
+        animes.add(anime);
+        return anime;
     }
 }
