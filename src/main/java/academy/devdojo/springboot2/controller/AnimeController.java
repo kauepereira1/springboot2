6eed5e9556d0bd7a70c6e378244b4b52dd9e4e98
Kaue Pereira <kauepereira_@outlook.com>
2024-06-21 14:05:47 -0300

    feat: implementando o metodo delete, aula 10

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -35,4 +35,11 @@ public class AnimeController {
     public ResponseEntity<Anime> save(@RequestBody Anime anime) {
         return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
     }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id) {
+        animeService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
